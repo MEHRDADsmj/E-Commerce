40508ca12a73276d3c6864f91c50df369bca6952
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-04-28 10:57:54 +0330

    Connect UserService to postgre

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<UserDbContext>(options =>
+                                             {
+                                                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+                                             });
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
