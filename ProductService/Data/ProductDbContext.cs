ed96d71157b8fd6c86f78ffb9ad6b8e40b4fdd7c
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-04-28 11:00:42 +0330

    Connect ProductService to postgre

---
+++ ProductService/Data/ProductDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductService.Data;
+
+public class ProductDbContext : DbContext
+{
+    public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+    {
+        
+    }
+}
--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ProductService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ProductDbContext>(options =>
+                                                {
+                                                    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+                                                });
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
