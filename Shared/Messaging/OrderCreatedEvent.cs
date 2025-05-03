aa52397af9e9c25fe17f466a14f7601894a49f7d
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-03 15:39:33 +0330

    Add Shared class library

---
+++ Shared/Messaging/OrderCreatedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Messaging;
+
+public class OrderCreatedEvent
+{
+    public Guid OrderId { get; set; }
+    public decimal Amount { get; set; }
+    public Guid UserId { get; set; }
+}
