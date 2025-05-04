cd2941fe4b3110bbc96250d0c3cc3fe52aa9a287
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-04 10:50:52 +0330

    Add IUserRepository interface

---
+++ UserService/Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using UserService.Domain.Entities;
+
+namespace UserService.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id);
+    Task<User?> GetByEmailAsync(string email);
+    Task AddAsync(User user);
+}
