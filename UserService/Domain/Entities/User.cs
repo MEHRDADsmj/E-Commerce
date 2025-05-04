db835b3b4510edf8224f4fa473b8b9ca831d36b1
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-04 10:48:09 +0330

    Add User entity

---
+++ UserService/Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Domain.Entities;
+
+public class User
+{
+    [Key] public Guid Id { get; set; }
+    [Required] public string Email { get; set; }
+    [Required] public string HashedPassword { get; set; }
+    [Required] public string FullName { get; set; }
+    DateTime CreatedAt { get; set; }
+}
