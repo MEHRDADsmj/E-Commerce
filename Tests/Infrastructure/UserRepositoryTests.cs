344245237f59c9444d3d7b40a79477485873f4cf
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-08 18:58:17 +0330

    Add integration tests for user repository

---
+++ Tests/Infrastructure/UserRepositoryTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Infrastructure.Repositories;
+using Testcontainers.PostgreSql;
+using UserService.Data;
+using UserService.Domain.Entities;
+
+namespace Tests.Infrastructure;
+
+public class UserRepositoryTests : IAsyncLifetime
+{
+    private UserRepository _userRepository;
+    private PostgreSqlContainer _dbContainer;
+
+    public async Task InitializeAsync()
+    {
+        _dbContainer = new PostgreSqlBuilder().Build();
+        await _dbContainer.StartAsync();
+
+        var options = new DbContextOptionsBuilder<UserDbContext>();
+        options.UseNpgsql(_dbContainer.GetConnectionString());
+
+        var dbContext = new UserDbContext(options.Options);
+        await dbContext.Database.MigrateAsync();
+        _userRepository = new UserRepository(dbContext);
+    }
+
+    [Fact]
+    public async Task AddAsync_ShouldAddUser()
+    {
+        var user = new User()
+                   {
+                       Email = "test@test.com",
+                       Id = Guid.NewGuid(),
+                       CreatedAt = DateTime.UtcNow,
+                       FullName = "John Doe",
+                       HashedPassword = "password",
+                   };
+        await _userRepository.AddAsync(user);
+        var result = await _userRepository.GetByIdAsync(user.Id);
+        Assert.NotNull(result);
+        Assert.Equal(user.Email, result.Email);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnUser()
+    {
+        var user = new User()
+                   {
+                       Email = "test@test.com",
+                       Id = Guid.NewGuid(),
+                       CreatedAt = DateTime.UtcNow,
+                       FullName = "John Doe",
+                       HashedPassword = "password",
+                   };
+        await _userRepository.AddAsync(user);
+        var result = await _userRepository.GetByIdAsync(user.Id);
+        Assert.NotNull(result);
+        Assert.Equal(user.Id, result.Id);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnNull()
+    {
+        var result = await _userRepository.GetByIdAsync(Guid.NewGuid());
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task GetByEmailAsync_ShouldReturnUser()
+    {
+        var user = new User()
+                   {
+                       Email = "test@test.com",
+                       Id = Guid.NewGuid(),
+                       CreatedAt = DateTime.UtcNow,
+                       FullName = "John Doe",
+                       HashedPassword = "password",
+                   };
+        await _userRepository.AddAsync(user);
+        var result = await _userRepository.GetByEmailAsync(user.Email);
+        Assert.NotNull(result);
+        Assert.Equal(user.Email, result.Email);
+    }
+
+    [Fact]
+    public async Task GetByEmailAsync_ShouldReturnNull()
+    {
+        var result = await _userRepository.GetByEmailAsync("test@test.com");
+        Assert.Null(result);
+    }
+
+    public Task DisposeAsync()
+    {
+        return _dbContainer.DisposeAsync().AsTask();
+    }
+}
---
+++ UserService/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+using UserService.Domain.Entities;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly UserDbContext _context;
+    
+    public UserRepository(UserDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(user => user.Id == id);
+        return user;
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(user => user.Email == email);
+        return user;
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+}
