8336790d84c24b8f86da5dac6f30a83894eb561e
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-13 16:31:53 +0330

    Add cart repository interface

---
+++ CartService/Domain/Interfaces/ICartRepository.cs
@@ -0,0 +1,12 @@
+﻿using CartService.Domain.Entities;
+
+namespace CartService.Domain.Interfaces;
+
+public interface ICartRepository
+{
+    Task<Cart?> GetAsync(Guid userId);
+    Task AddItemAsync(Guid userId, Guid productId, int quantity);
+    Task RemoveItemAsync(Guid userId, Guid productId);
+    Task UpdateItemQuantityAsync(Guid userId, Guid productId, int newQuantity);
+    Task ClearCartAsync(Guid userId);
+}
