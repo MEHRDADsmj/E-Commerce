e1acf0b9ec70dbcb0447727bcd59a523e68459cb
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-13 16:31:03 +0330

    Add domain entities to cart service

---
+++ CartService/Domain/Entities/Cart.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CartService.Domain.Entities;
+
+public class Cart
+{
+    [Key] public Guid UserId { get; set; }
+    public List<CartItem> Items { get; set; } = new List<CartItem>();
+}
---
+++ CartService/Domain/Entities/CartItem.cs
@@ -0,0 +1,7 @@
+﻿namespace CartService.Domain.Entities;
+
+public class CartItem
+{
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+}
