9a86e0fed73136548390bc7521c5ca87fad489b9
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-26 17:53:13 +0330

    Add order repository interface

---
+++ OrderService/Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using OrderService.Domain.Entities;
+
+namespace OrderService.Domain.Interfaces;
+
+public interface IOrderRepository
+{
+    Task AddAsync(Order order);
+    Task<Order?> GetByIdAsync(Guid orderId);
+    Task<List<Order>> GetByUserIdAsync(Guid userId);
+}
