2a5da082f494ae66486f0cad35c0e453686d801f
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-28 14:55:16 +0330

    Add IProductRepository interface

--- ProductService/Domain/Entities/Product.cs
+++ ProductService/Domain/Entities/Product.cs
@@ -6,6 +6,6 @@ public class Product
 {
     [Key] public Guid Id { get; set; }
     [Required] public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     [Required] public decimal UnitPrice { get; set; }
 }
---
+++ ProductService/Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProductService.Domain.Entities;
+
+namespace ProductService.Domain.Interfaces;
+
+public interface IProductRepository
+{
+    Task<Product> GetByIdAsync(Guid id);
+    Task<List<Product>> GetAllAsync();
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+    Task DeleteAsync(Guid id);
+    Task<IEnumerable<Product>> GetBulkAsync(IEnumerable<Product> products);
+}
