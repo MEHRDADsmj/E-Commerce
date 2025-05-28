b469b2e2f72832c393bed1d0a344bdba81a17eb1
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-28 15:27:24 +0330

    Add delete product use case

---
+++ ProductService/Application/Products/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Shared.Data;
+
+namespace ProductService.Application.Products.Commands.DeleteProduct;
+
+public record DeleteProductCommand(Guid ProductId) : IRequest<Result<bool>>;
---
+++ ProductService/Application/Products/Commands/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using ProductService.Domain.Interfaces;
+using Shared.Data;
+
+namespace ProductService.Application.Products.Commands.DeleteProduct;
+
+public class DeleteProductHandler : IRequestHandler<DeleteProductCommand, Result<bool>>
+{
+    private readonly IProductRepository _productRepository;
+
+    public DeleteProductHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<Result<bool>> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _productRepository.DeleteAsync(request.ProductId);
+            return Result<bool>.Success(true);
+        }
+        catch (Exception e)
+        {
+            return Result<bool>.Failure(e.Message);
+        }
+    }
+}
