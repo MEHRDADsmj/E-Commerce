62b977baabb4ed95b0e557e6c583e799cd05235e
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-28 13:09:54 +0330

    Add product entity

---
+++ ProductService/Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductService.Domain.Entities;
+
+public class Product
+{
+    [Key] public Guid Id { get; set; }
+    [Required] public string Name { get; set; }
+    public string Description { get; set; }
+    [Required] public decimal UnitPrice { get; set; }
+}
