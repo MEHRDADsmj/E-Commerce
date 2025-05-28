e33a93d70eec849783a23ccf005ffc36ce486db7
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-05-28 15:33:03 +0330

    Add get products bulk use case

---
+++ ProductService/Application/Products/Commands/GetBulk/GetProductsBulkHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using ProductService.Domain.Entities;
+using ProductService.Domain.Interfaces;
+using Shared.Data;
+
+namespace ProductService.Application.Products.Commands.GetBulk;
+
+public class GetProductsBulkHandler : IRequestHandler<GetProductsBulkQuery, Result<IEnumerable<Product>>>
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetProductsBulkHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+    
+    public async Task<Result<IEnumerable<Product>>> Handle(GetProductsBulkQuery request, CancellationToken cancellationToken)
+    {
+        if (!request.Products.Any())
+        {
+            return Result<IEnumerable<Product>>.Failure("No Ids provided");
+        }
+        var products = await _productRepository.GetBulkAsync(request.Products);
+        var enumerable = products.ToList();
+        if (enumerable.Count == 0)
+        {
+            return Result<IEnumerable<Product>>.Failure("No Products found");
+        }
+        return Result<IEnumerable<Product>>.Success(enumerable);
+    }
+}
