5cac0f9764e0e5eac918516a32939b525f0c55b9
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-06-06 12:40:26 +0330

    Add get order by user id use case

---
+++ OrderService/Application/Orders/Commands/GetOrderByUserId/GetOrderByUserIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using OrderService.Domain.Entities;
+using OrderService.Domain.Interfaces;
+using Shared.Data;
+
+namespace OrderService.Application.Orders.Commands.GetOrderByUserId;
+
+public class GetOrderByUserIdHandler : IRequestHandler<GetOrderByUserIdQuery, Result<IEnumerable<Order>>>
+{
+    private readonly IOrderRepository _orderRepository;
+
+    public GetOrderByUserIdHandler(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+    
+    public async Task<Result<IEnumerable<Order>>> Handle(GetOrderByUserIdQuery request, CancellationToken cancellationToken)
+    {
+        var orders = await _orderRepository.GetByUserIdAsync(request.UserId);
+        if (orders.Count == 0)
+        {
+            return Result<IEnumerable<Order>>.Failure("Orders not found");
+        }
+        
+        return Result<IEnumerable<Order>>.Success(orders);
+    }
+}
---
+++ OrderService/Application/Orders/Commands/GetOrderByUserId/GetOrderByUserIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using OrderService.Domain.Entities;
+using Shared.Data;
+
+namespace OrderService.Application.Orders.Commands.GetOrderByUserId;
+
+public record GetOrderByUserIdQuery(Guid UserId) : IRequest<Result<IEnumerable<Order>>>;
