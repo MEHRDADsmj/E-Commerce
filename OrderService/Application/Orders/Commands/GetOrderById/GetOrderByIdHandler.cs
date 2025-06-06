7f37d626eeda8175bbb7b9db86d8199e5e7ae96f
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-06-06 12:36:12 +0330

    Add get order by id use case

---
+++ OrderService/Application/Orders/Commands/GetOrderById/GetOrderByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using OrderService.Domain.Entities;
+using OrderService.Domain.Interfaces;
+using Shared.Data;
+
+namespace OrderService.Application.Orders.Commands.GetOrderById;
+
+public class GetOrderByIdHandler : IRequestHandler<GetOrderByIdQuery, Result<Order>>
+{
+    private readonly IOrderRepository _orderRepository;
+
+    public GetOrderByIdHandler(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+    
+    public async Task<Result<Order>> Handle(GetOrderByIdQuery request, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepository.GetByIdAsync(request.OrderId);
+        if (order == null)
+        {
+            return Result<Order>.Failure("Order not found");
+        }
+        
+        return Result<Order>.Success(order);
+    }
+}
---
+++ OrderService/Application/Orders/Commands/GetOrderById/GetOrderByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using OrderService.Domain.Entities;
+using Shared.Data;
+
+namespace OrderService.Application.Orders.Commands.GetOrderById;
+
+public record GetOrderByIdQuery(Guid OrderId) : IRequest<Result<Order>>;
