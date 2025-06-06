95dafff2c18b462c7252d6c0fb540e89b021529e
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-06-06 12:47:37 +0330

    Add mark order as failed use case

---
+++ OrderService/Application/Orders/Commands/MarkOrderAsFailed/MarkOrderAsFailedCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Shared.Data;
+
+namespace OrderService.Application.Orders.Commands.MarkOrderAsFailed;
+
+public record MarkOrderAsFailedCommand(Guid Id) : IRequest<Result<bool>>;
---
+++ OrderService/Application/Orders/Commands/MarkOrderAsFailed/MarkOrderAsFailedHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using OrderService.Domain.Interfaces;
+using Shared.Data;
+
+namespace OrderService.Application.Orders.Commands.MarkOrderAsFailed;
+
+public class MarkOrderAsFailedHandler : IRequestHandler<MarkOrderAsFailedCommand, Result<bool>>
+{
+    private readonly IOrderRepository _orderRepository;
+
+    public MarkOrderAsFailedHandler(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+    
+    public async Task<Result<bool>> Handle(MarkOrderAsFailedCommand request, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepository.GetByIdAsync(request.Id);
+        if (order == null)
+        {
+            return Result<bool>.Failure("Order not found");
+        }
+        
+        order.MarkAsFailed();
+        await _orderRepository.SaveAsync();
+        
+        return Result<bool>.Success(true);
+    }
+}
