e3185c3a2ea973b1994d8dbd5d4f4fb2645c60f7
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-06-21 17:00:07 +0330

    Add primary key to order item

--- OrderService/Domain/Entities/OrderItem.cs
+++ OrderService/Domain/Entities/OrderItem.cs
@@ -4,7 +4,8 @@ namespace OrderService.Domain.Entities;
 
 public class OrderItem
 {
-    [Key] public Guid ProductId { get; private set; }
+    [Key] public Guid Id { get; private set; }
+    public Guid ProductId { get; private set; }
     [MaxLength(64)] public string ProductName { get; private set; }
     public decimal UnitPrice { get; private set; }
     [Range(1, int.MaxValue)] public int Quantity { get; private set; }
---
+++ OrderService/Migrations/20250621132205_AddOrderItemIdAsKey.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderItemIdAsKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "OrderItem",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "OrderItem");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem",
+                column: "ProductId");
+        }
+    }
+}
