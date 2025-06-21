d03d6b13c1a6bfd3d385b56980cd72921f81432a
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-06-21 17:04:55 +0330

    Add create order endpoint

---
+++ OrderService/Presentation/Controllers/OrdersController.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Application.Orders.Commands.CreateOrder;
+
+namespace OrderService.Presentation.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    
+    public OrdersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [AllowAnonymous]
+    [HttpGet("health")]
+    public IActionResult GetHealth()
+    {
+        return Ok("Orders Healthy");
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateOrder()
+    {
+        var userId = HttpContext.User.Claims.FirstOrDefault(claim => claim.Type == "user_id")?.Value;
+        if (userId == null)
+        {
+            return Unauthorized();
+        }
+
+        string jwtToken = HttpContext.Request.Headers.Authorization.ToString().Split()[1];
+        var command = new CreateOrderCommand(Guid.Parse(userId), jwtToken);
+        var result = await _mediator.Send(command);
+
+        if (result.IsSuccess)
+        {
+            return Ok(result.Value);
+        }
+        return BadRequest(result.ErrorMessage);
+    }
+}
