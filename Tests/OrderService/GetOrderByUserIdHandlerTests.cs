09f6b6ad0da8aaa2dd8c8a11a08d91ae45a34d7e
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-06-23 11:27:22 +0330

    Add tests for get order by user id use case

---
+++ Tests/OrderService/GetOrderByUserIdHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using Bogus;
+using FluentAssertions;
+using Moq;
+using OrderService.Application.Orders.Commands.GetOrderByUserId;
+using OrderService.Domain.Entities;
+using OrderService.Domain.Interfaces;
+
+namespace Tests.OrderService;
+
+public class GetOrderByUserIdHandlerTests
+{
+    private readonly Mock<IOrderRepository> _orderRepositoryMock;
+    private readonly GetOrderByUserIdHandler _handler;
+
+    public GetOrderByUserIdHandlerTests()
+    {
+        _orderRepositoryMock = new Mock<IOrderRepository>();
+        _handler = new GetOrderByUserIdHandler(_orderRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFalse_WhenOrderIsNotFound()
+    {
+        _orderRepositoryMock.Setup(repo => repo.GetByUserIdAsync(It.IsAny<Guid>()))
+                            .ReturnsAsync(new List<Order>());
+        
+        var query = new GetOrderByUserIdQuery(Guid.NewGuid());
+        var result = await _handler.Handle(query, CancellationToken.None);
+        
+        result.IsSuccess.Should().BeFalse();
+        _orderRepositoryMock.Verify(repo => repo.GetByUserIdAsync(It.IsAny<Guid>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnTrue_WhenOrderIsFound()
+    {
+        _orderRepositoryMock.Setup(repo => repo.GetByUserIdAsync(It.IsAny<Guid>()))
+                            .ReturnsAsync(new Faker<Order>().Generate(3));
+        
+        var query = new GetOrderByUserIdQuery(Guid.NewGuid());
+        var result = await _handler.Handle(query, CancellationToken.None);
+        
+        result.IsSuccess.Should().BeTrue();
+        _orderRepositoryMock.Verify(repo => repo.GetByUserIdAsync(It.IsAny<Guid>()), Times.Once);
+    }
+}
