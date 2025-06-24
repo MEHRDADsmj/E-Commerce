b3eabcc25d2494574b21d3c77219b0d9ef09195b
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-06-24 11:35:26 +0330

    Add entities for payment service

---
+++ PaymentService/Domain/Entities/Payment.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentService.Domain.Entities;
+
+public class Payment
+{
+    [Key] public Guid OrderId { get; private set; }
+    public Guid UserId { get; private set; }
+    public decimal TotalPrice { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public PaymentStatus Status { get; private set; }
+
+    public Payment(Guid orderId, Guid userId, decimal totalPrice)
+    {
+        OrderId = orderId;
+        UserId = userId;
+        TotalPrice = totalPrice;
+        Status = PaymentStatus.Pending;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void MarkAsCompleted()
+    {
+        Status = PaymentStatus.Completed;
+    }
+
+    public void MarkAsFailed()
+    {
+        Status = PaymentStatus.Failed;
+    }
+}
