d67e9d98c0f0fedf36c56531d7536fb0ee496fe2
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-07-01 20:51:27 +0330

    Add required services to payment service

--- PaymentService/Program.cs
+++ PaymentService/Program.cs
@@ -1,7 +1,9 @@
+using System.Reflection;
 using System.Text.Json;
 using PaymentService.Application.Interfaces;
+using PaymentService.Domain.Interfaces;
 using PaymentService.Infrastructure.Messaging;
-using RabbitMQ.Client;
+using PaymentService.Infrastructure.PaymentProcessing;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +13,13 @@ builder.Services.AddControllers().AddJsonOptions(options =>
                                                  {
                                                      options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
                                                  });
+builder.Services.AddMediatR(config =>
+                            {
+                                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                            });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IPaymentProcessor, FakePaymentProcessor>();
 builder.Services.AddSingleton<IEventPublisher, RabbitMqPublisher>();
 builder.Services.AddHostedService<RabbitMqConsumer>();
 
