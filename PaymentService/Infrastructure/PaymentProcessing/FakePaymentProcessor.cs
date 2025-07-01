10ad2fcefde97e547ae8b60af3c07e211106f336
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-07-01 20:40:35 +0330

    Implement payment processor

---
+++ PaymentService/Infrastructure/PaymentProcessing/FakePaymentProcessor.cs
@@ -0,0 +1,15 @@
+﻿using PaymentService.Domain.Entities;
+using PaymentService.Domain.Interfaces;
+
+namespace PaymentService.Infrastructure.PaymentProcessing;
+
+public class FakePaymentProcessor : IPaymentProcessor
+{
+    public async Task<Payment> ProcessAsync(Guid orderId, Guid userId, decimal totalPrice)
+    {
+        await Task.Delay(3000);
+        var payment = new Payment(orderId, userId, totalPrice);
+        payment.MarkAsCompleted();
+        return payment;
+    }
+}
