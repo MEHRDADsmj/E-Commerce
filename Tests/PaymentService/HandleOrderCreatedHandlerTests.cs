de8156014cce1976527cc2cb52a31825e5a74214
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2025-07-06 12:42:38 +0330

    Add failing test for handle order created use case

--- Tests/PaymentService/HandleOrderCreatedHandlerTests.cs
+++ Tests/PaymentService/HandleOrderCreatedHandlerTests.cs
@@ -37,7 +37,28 @@ public class HandleOrderCreatedHandlerTests
         var res = await _handler.Handle(command, CancellationToken.None);
 
         res.IsSuccess.Should().BeTrue();
-        _paymentProcessorMock.Verify(processor => processor.ProcessAsync(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<decimal>()), Times.Once);
+        _paymentProcessorMock.Verify(processor => processor.ProcessAsync(It.IsAny<Guid>(), 
+                                                                         It.IsAny<Guid>(), It.IsAny<decimal>()), Times.Once);
+        _eventPublisherMock.Verify(publisher => publisher.PublishAsync(It.IsAny<IEvent>(), It.IsAny<string>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFalse_WhenPaymentIsNotProcessed()
+    {
+        var payment = new Payment(Guid.NewGuid(), Guid.NewGuid(), new Faker().Random.Decimal());
+        payment.MarkAsFailed();
+        _paymentProcessorMock.Setup(payment => payment.ProcessAsync(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<decimal>()))
+                             .ReturnsAsync(payment);
+        
+        _eventPublisherMock.Setup(publisher => publisher.PublishAsync(It.IsAny<PaymentCompletedEvent>(), It.IsAny<string>()))
+                           .Returns(Task.CompletedTask);
+        
+        var command = new HandleOrderCreatedCommand(Guid.NewGuid(), Guid.NewGuid(), new Faker().Random.Decimal());
+        var res = await _handler.Handle(command, CancellationToken.None);
+
+        res.IsSuccess.Should().BeFalse();
+        _paymentProcessorMock.Verify(processor => processor.ProcessAsync(It.IsAny<Guid>(), 
+                                                                         It.IsAny<Guid>(), It.IsAny<decimal>()), Times.Once);
         _eventPublisherMock.Verify(publisher => publisher.PublishAsync(It.IsAny<IEvent>(), It.IsAny<string>()), Times.Once);
     }
 }
